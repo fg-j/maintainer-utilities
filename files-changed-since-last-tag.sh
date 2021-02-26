7a77bcbfe2a57f874a5643404992296d170ce8b5
Frankie Gallina-Jones <frankieg@vmware.com>
2021-02-26 11:05:12 -0500

    add scripts for looking at changes since latest tag

---
+++ files-changed-since-last-tag.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+set -eu
+set -o pipefail
+
+# readonly PROGDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+# readonly BUILDPACKDIR="$(cd "${PROGDIR}/.." && pwd)"
+
+function main() {
+  compare_files "${@}"
+}
+
+function compare_files(){
+  local repo
+  while [[ "${#}" != 0 ]]; do
+    repo="${1}"
+    shift 1
+
+    # go to the repo
+    printf "Comparing %s...\n\n" "${repo}"
+    read -p "Press enter to continue"
+
+    pushd "${HOME}/workspace/paketo-buildpacks/${repo}" > /dev/null
+    # checkout main and pull, fetch all tags
+    git co main
+    git pull origin main -r
+    git fetch -a
+    # do the comparison
+    git diff $(git describe --tags `git rev-list --tags --max-count=1`) main --stat
+    popd > /dev/null
+  done
+}
+
+main "${@:-}"
